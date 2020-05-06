7d215f383171c48af6f00929e6e256f33d4d549a
Numair <numairahmed1@gmail.com>
2020-05-05 18:03:21 -0700

    complete layout of first class, Mortgage

--- mortgages_calculator.py
+++ mortgages_calculator.py
@@ -19,3 +19,33 @@ Requirements:
     - and a description of the mortgage (typically None)
 """
 
+def findPayment(loan, r, m):
+    """Assum: loan and r are floats, m an int.
+    Returns the monthly payment for a mortgage of size loan at a monthly rate of r for m months """
+    return loan*((r*(1+r)**m)/((1+r)**m-1)) #loan calculation equation
+
+class Mortgage(object):
+    """Abstract class for building different kinds of mortgages. This means that all of the below equations and methods (w/in this class) are common to all three types of mortgage lending schemes. """
+    def __init__(self, loan, annRate, months):
+        """Assumes loan and annRate are floats, months an int.
+        Creates a new mortgage of size loan, duration months, and annual rate annRate """
+        self.loan = loan
+        self.rate = annRate/12
+        self.months = months
+        self.paid = [0.0] #start with $0.00 paid of the mortgage
+        self.outstanding = [loan] #I'm guessing this is in a list bc it will catalog outstanding loan balances every month into a list
+        self.payment = findPayment(loan, self.rate, months) #why do we need to init with this inside __init__ ?
+        self.legend = None #description of mortgage. Will this be changed later?
+
+    def makePayment(self):
+        """Make a payment """
+        self.paid.append(self.payment) #my hunch was correct: each payment is appended to the end of the self.paid list
+        reduction = self.payment - self.outstanding[-1]*self.rate #calculate how much the payment will reduce the outstanding PRINCIPLE balance
+        self.outstanding.append(self.outstanding[-1] - reduction) #apply that reduction to the outstanding PRINCIPLE balance
+
+    def getTotalPaid(self):
+        """Return the total amount paid so far"""
+        return sum(self.paid) #because all payments are tabulated to this list, you can sum down the whole list to get a total amount paid
+
+    def __str__(self):
+        return self.legend #why is this needed?
