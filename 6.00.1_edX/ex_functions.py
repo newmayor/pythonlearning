d0133ea393f4aa7251fc3785542af9fea863dde5
Numair <numairahmed1@gmail.com>
2020-05-05 17:41:43 -0700

    first attempt at building glasses and using inheritance

---
+++ 6.00.1_edX/ex_functions.py
@@ -0,0 +1,6 @@
+def absolute(a):
+    return abs(a)
+
+testList = [1, -4, 8, -9]
+
+print(absolute(testList))
---
+++ mortgages_calculator.py
@@ -0,0 +1,21 @@
+"""Numair Ahmed - self learning
+Textbook: Intro to Computation and Programming Using Python by John Guttag
+Ch. 8.4: Mortgages, an Extended Example
+This exercise will examine the costs of three kinds of loans:
+ - A fixed rate mortgage with no points
+ - A fixed rate mortgage with points
+ - A mortgage with an initial teaser rate followed by a higher rate for the duration
+
+Requirements:
+1. include a Mortgage class
+2. include a subclass for each type of mortgage mentioned above
+3. __init__ will have instance variables corresponding to: 
+    - the initial loan amount, 
+    - the monthly interest rate, 
+    - the duration of the loan in months, 
+    - a list of payments that have been made at the start of each month,
+    - a list with the balance of the loan that is outstanding each month,
+    - the amount of money to be paid each month,
+    - and a description of the mortgage (typically None)
+"""
+
