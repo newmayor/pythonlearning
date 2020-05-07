50953bb06cbce469f1c85c494d8661c0b2026eae
Numair <numairahmed1@gmail.com>
2020-05-06 22:26:54 -0700

    complete main body code

--- mortgages_calculator.py
+++ mortgages_calculator.py
@@ -78,5 +78,22 @@ class TwoRate(Mortgage):
                  self.months - self.teaserMonths) #calculate the loan payment findPayment using the outstanding PRINCIPLE balance, the self.rate when it's defined after the teaserRate has expired, for the months remaining after the teaserMonths duration is over.
         Mortgage.makePayment(self)
 
+def compareMortgages(amt, years, fixedRate, pts, ptsRate, varRate1, varRate2, varMonths):
+    totMonths = years*12
+    fixed1 = Fixed(amt, fixedRate, totMonths)
+    fixed2 = FixedWithPts(amt, ptsRate, totMonths, pts)
+    twoRate = TwoRate(amt, varRate2, totMonths, varRate1, varMonths)
+    morts = [fixed1, fixed2, twoRate]
+
+    for m in range(totMonths):
+        for mort in morts:
+            mort.makePayment()
+    for m in morts:
+        print(m)
+        print(' Total payments = $' + str(int(m.getTotalPaid())))
+
+compareMortgages(amt=200000, years=30, fixedRate=0.07, pts=3.25, ptsRate=0.05,\
+    varRate1=0.045, varRate2=0.095, varMonths=48)
+
 
 
