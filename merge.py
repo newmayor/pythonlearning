88ae842cee951befe20198e3e781ad7a8bb69b93
Numair <numairahmed1@gmail.com>
2020-05-09 14:53:34 -0700

    learning search and sort algos

---
+++ merge.py
@@ -0,0 +1,34 @@
+
+def merge(left, right, compare): 
+    """Assumes left and right are sorted lists and compare defines and ordering on the elements.
+    Returns a new sorted (by compare) list containing the same elements as (left + right) would contain. """
+
+    result = [] #initiate the return object
+    i,j = 0,0 #starting position?
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[i]): #compare is a function?
+            result.append(left[i])
+            result += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while (i < len(left)):
+        result.append(left[i]) #whats happening here? Just building another list as a copy of left?
+        i += 1
+    while (j < len(right)):
+        result.append(right[j]) #looks like this while loop and the one before it (for left) are just appending the whole left and right lists at the end of result after already having sorted the lists in the very first while loop. Why is that?
+        j += 1
+    return result
+
+
+def mergeSort(L, compare = lambda x, y: x<y):
+    """Assumes L is a list, compare defines and ordering on elements of L.
+    Returns a new sorted list with the same elements as L.
+    What the heck is lambda? """
+    if len(L) < 2:
+        return L[:] #list is too short, so it's already sorted
+    else:
+        middle = len(L)//2 #what's the double / do? Is it rounded division?
+        left = mergeSort(L[:middle], compare)
+        right = mergeSort(L[middle:], compare)
+        return merge(left, right, compare)
--- selectionSort.py
+++ selectionSort.py
@@ -10,3 +10,4 @@ def selSort(L):
                 #swap position of elements
                 L[suffixStart], L[i] = L[i], L[suffixStart]
         suffixStart += 1
+    #this program consists of two loops that increment thru a length of list. The inside one is O(len(L)) and the outside one is O(len(L)). Therefore, this program has complexity O(len(L^2)). This is known as quadratic time complexity.
